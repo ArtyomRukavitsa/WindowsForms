64ec90575afaff006d246018c9037a19f72b34c6
Артем (Desktop) <57802035+ArtyomRukavitsa@users.noreply.github.com>
2020-05-12 10:20:58 +0300

    Практика 15.1 и 15.2

---
+++ Практика15.2/Практика15.2/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практика15._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = $"Указанное число миль равно: {double.Parse(textBox1.Text) * 1.609:f2} км.";
+        }
+    }
+}
