8b10f7f1d570b320a9ec8b45e42f4ef28be82cf8
Артем (Desktop) <57802035+ArtyomRukavitsa@users.noreply.github.com>
2020-05-20 22:25:38 +0300

    Практика 16, вариант 5

---
+++ Практика16/Практика17/Class1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика17
+{
+    class NumeralSystem
+    {
+        public NumeralSystem() { }
+
+        public static string ConvertToTen(string number)
+        {
+            int result = 0;
+            for (int i = 0; i < number.Length; i++)
+            {
+                result += Convert.ToInt32(Convert.ToString(number[number.Length - i - 1])) 
+                    * (int)Math.Pow(2, i);
+            }
+            return Convert.ToString(result);
+        }
+
+        static string ReverseNumbers(string number)
+        {
+            string result = "";  // Итоговый результат
+            for (int i = 0; i < number.Length; i++)
+            {
+                result += number[number.Length - i - 1];
+            }
+            return result;
+        } 
+        public static string ConvertToEight(string number)
+        {
+            int current; // Сумма тройки
+            string result = ""; // Перевернутый результат
+            if (number.Length % 3 == 2) number = "0" + number; // Дополнение до длины, кратной 3
+            else if (number.Length % 3 == 1) number = "00" + number; // Дополнение до длины, кратной 3
+            for (int i = 0; i < number.Length; i += 3) // Разделил число на тройки
+            {
+                current = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    current += Convert.ToInt32(Convert.ToString(number[number.Length - i - j - 1]))
+                    * (int)Math.Pow(2, j);
+                }
+                result += Convert.ToString(current); // Складываем в обратном порядке
+            }
+            string result_good = ReverseNumbers(result);
+            return result_good;
+        }
+
+        
+        public static string ConvertToSixteen(string number)
+        {
+            string[] numbers = { "A", "B", "C", "D", "E", "F" };
+            int current; // Сумма четверки
+            string result = ""; // Перевернутый результат
+            if (number.Length % 4 == 3) number = "0" + number; // Дополнение до длины, кратной 4
+            else if (number.Length % 4 == 2) number = "00" + number; // Дополнение до длины, кратной 4
+            else if (number.Length % 4 == 1) number = "000" + number; // Дополнение до длины, кратной 4
+            for (int i = 0; i < number.Length; i += 4) // Разделил число на четверки
+            {
+                current = 0;
+                for (int j = 0; j < 4; j++)
+                {
+                    current += Convert.ToInt32(Convert.ToString(number[number.Length - i - j - 1]))
+                    * (int)Math.Pow(2, j);
+                }
+                if (current <= 9) result += Convert.ToString(current); // Складываем в обратном порядке
+                else result += numbers[current % 10]; // Сумма четверки больше 10, прибавляем букву
+            }
+            string result_good = ReverseNumbers(result);  // Итоговый результат]
+            return result_good;
+        }
+    }
+}
---
+++ Практика16/Практика17/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практика17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Do_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text.Contains("2") || textBox1.Text.Contains("3") ||
+                    textBox1.Text.Contains("4") || textBox1.Text.Contains("5") ||
+                    textBox1.Text.Contains("6") || textBox1.Text.Contains("7") ||
+                    textBox1.Text.Contains("8") || textBox1.Text.Contains("9"))
+                    throw new Exception("Число не в 2ой системе");
+                if (radioButton1.Checked) // 8
+                {
+                    textBox2.Text = NumeralSystem.ConvertToEight(textBox1.Text);
+                }
+                else if (radioButton2.Checked) // 10
+                {
+                    textBox2.Text = NumeralSystem.ConvertToTen(textBox1.Text);
+                }
+                else if (radioButton3.Checked) // 16
+                {
+                    textBox2.Text = NumeralSystem.ConvertToSixteen(textBox1.Text);
+                }
+            }
+            catch (FormatException)
+            {
+                textBox2.Text = "Вы ввели не число!";
+            }
+            catch (Exception Error)
+            {
+                textBox2.Text = Error.Message;
+            }
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
